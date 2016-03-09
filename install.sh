e4f12f9169790db47d9082d874c15cc997f57294
Alex Conrey <alex@conrey.us>
2016-03-09 14:51:08 -0600

    added tracker blocking

--- install.sh
+++ install.sh
@@ -2,18 +2,22 @@
 # Be sure to include /etc/bind/named.conf.blocked in /etc/bind/named.conf
 DIR=$(pwd)
 CONFFILE="/etc/bind/named.conf.blocked"
-
+FILES=('/etc/bind/named.conf.blocked' '/etc/bind/named.conf.trackers')
 if [ ! -d "$DIR" ]; then
 	mkdir -p $DIR
 	apt-get install bind9
-	echo "include '$CONFFILE';" >> /etc/bind/named.conf
+	for i in ${FILES[@]}; do
+		echo "include '$i';" >> /etc/bind/named.conf
+	done
 	cp $DIR/conf/null.zone.file /etc/bind
-	echo '* 12 * * * '$DIR'/update_lists.sh' 
+	echo '* 12 * * * '$DIR'/update_lists.sh'  >> /etc/cron.d/afh
+	echo '* */6 * * * '$DIR'/update_trackers.sh' >> /etc/cron.d/afh
 else
 	echo "Looks like this has already been installed. Skipping including files."
 fi
 
 bash $DIR/update_lists.sh
+bash $DIR/update_trackers.sh
 
 service bind9 restart
 
---
+++ update_trackers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Updates the lists and formats for named.conf.blocked use
+DIR="/opt/adblocker/trackers"
+ZONEFILE="/etc/bind/null.zone.file"
+if [ ! -d "$DIR" ]; then
+	mkdir -p $DIR
+fi
+
+if [ -a /etc/bind/named.conf.blocked ]; then
+	rm /etc/bind/named.conf.trackers
+fi
+
+lists=('https://raw.githubusercontent.com/quidsup/notrack/master/trackers.txt')
+for i in ${lists[@]}; do
+	NAME=$(echo $i | sed 's/http:\/\///g' | sed 's/https:\/\///g' | cut -d"/" -f1)
+	echo "Fetching $NAME ..."
+	curl $i -o $DIR/$NAME
+done
+
+for i in $(ls $DIR); do
+	for t in $(cat $DIR/$i | grep -v '^#' | awk '{ print $1 }' | sort | uniq); do
+		echo 'zone "'"$t"'" { type master; notify no; file "'"$ZONEFILE"'"; };' >> /etc/bind/named.conf.trackers.tmp
+	done
+done
+
+cat /etc/bind/named.conf.trackers.tmp | sort | uniq > /etc/bind/named.conf.trackers
+
+service bind9 restart
